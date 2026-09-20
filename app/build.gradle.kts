36a60408eadb0b5943e6a5b6e0a63bb9c3a4e4b9
Ishan <www.ishanchoudhury@gmail.com>
2026-09-20 09:08:27 +0530

    chore: set unique application ID for LastWaveX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.lastwave.app"
+        applicationId = "com.specttre404.lastwavex"
         minSdk = (project.findProperty("minSdk") as? String)?.toIntOrNull() ?: 29
         targetSdk = 35
         versionCode = 17
